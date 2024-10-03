1ba46765be59dd28164f033808b906c5262ad841
Misha <famm93@mail.ru>
2024-10-03 13:16:12 +0300

    add useLocalStorge

---
+++ src/components/News/News.tsx
@@ -0,0 +1,6 @@
+import styles from "./News.module.css";
+
+const News = () => {
+  return <div className={styles.news}></div>;
+};
+export default News;
---
+++ src/interfaces/enum.ts
@@ -0,0 +1,3 @@
+export enum EUseLocalStorage {
+  NEWS = "news",
+}
